7ab0fca7fc7702cf45716f467a9e31b7f7b940d5
DESKTOP-M2RDM21\akira <sunroad0418@gmail.com>
2018-01-03 17:08:01 +0800

    first 2018/01/03

---
+++ app/js/createtable.js
@@ -0,0 +1,44 @@
+
+var test =' "employees": ['+
+    '{ "MemberName":"John", "MemberNum":"abc123","ID":"abc","Date":"1987/07/09","Permission":"3" },'+
+    '{ "MemberName":"Steve", "MemberNum":"abc456","ID":"www","Date":"1987/07/19","Permission":"2" },'+
+   '{ "MemberName":"John", "MemberNum":"abc789","ID":"xyz","Date":"1987/08/09","Permission":"1" } ] }';
+
+
+function ShowTable (tablename){
+
+    var table = document.getElementById(tablename);
+   
+    
+    var obj = JSON.parse(test);
+    
+    var l = obj.length;
+
+    var attr = ["MemberName", "MemberNum", "ID", "Date", "Permission"];
+
+    for(var i=0;i<l;i++){
+
+        var tr = document.createElement("tr");
+        var td_arr = [];
+        for(var i=0;i<6;i++)td_arr.push(document.createElement("td"));
+
+        td_arr[0].appendChild(obj[i].MemberName);
+        td_arr[1].appendChild(obj[i].MemberNum);
+        td_arr[2].appendChild(obj[i].ID);
+        td_arr[3].appendChild(obj[i].Date);
+        td_arr[4].appendChild(obj[i].Permission);
+        td_arr[5].appendChild('<button>編輯</button><button>刪除</button>');
+        for(var j=0;j<6;j++)
+        tr.appendChild(td_arr[j]);
+        
+
+        table.append(tr);
+    }
+
+    
+
+
+}
+
+
+    
---
+++ app/js/login.js
@@ -0,0 +1,121 @@
+var request = require('request');
+var host = 'http://lei-k.ddns.net:8081';
+var login_options ;
+var logout_options = {
+    url: host + '/bim/api/staff/logout',
+    method: "DELETE",
+    headers: {
+        'User-Agent': 'request',
+        'authorization' : auth
+      },
+      json: true,   // <--Very important!!!
+};
+var update_info_options = {
+    url: host + '/bim/api/staff/update',
+    method: "POST",
+    headers: {
+        'User-Agent': 'request',
+        'authorization' : auth
+      },
+      json: true,   // <--Very important!!!
+      body: info
+};
+var get_info_options = {
+    url: host + '/bim/api/staff/info',
+    method: "GET",
+    headers: {
+        'User-Agent': 'request',
+        'authorization' : auth
+      },
+      json: true,   // <--Very important!!!
+  };
+  
+
+
+
+
+
+  function logout_callback(error, response, body) {
+    console.log("");
+    console.log("logout callback:");
+    console.log("http status code: " + response.statusCode);
+    console.log(body);
+  }
+
+  function do_logout(){
+    request(logout_options, logout_callback);
+  }
+
+  var info;
+
+  function update_info_callback(error, response, body){
+    console.log("");
+    console.log("update info callback:");
+    console.log("http status code: " + response.statusCode);
+    console.log(body);
+
+    do_logout();
+  }
+
+  function do_update_info(error, response, body){
+    info.emailAddress = 'aabbc!@gamil.com';
+    request(update_info_options, update_info_callback);
+  }
+
+  function get_info_callback(error, response, body){
+    console.log("");
+    console.log("get info callback:");
+    console.log("http status code: " + response.statusCode);
+    console.log(body);
+
+    info = body['content'];
+
+    do_update_info();
+  }
+
+  function do_get_info(error, response, body){
+    request(get_info_options, get_info_callback);
+  }
+
+  var auth;
+  var id;
+
+  function login_callback(error, response, body) {
+    console.log("login callback: ");
+    console.log("http status code: " + response.statusCode);
+    console.log(body);
+    
+    document.getElementById("xyz").innerHTML =  JSON.stringify(body);
+    
+   
+
+    if (!error && response.statusCode == 200) {
+      // 獲取用戶id、token拼接後作為簽證(auth)備用
+      id = body['content'].id;
+      auth = id + '_' + body['content'].token;
+      console.log("auth: " + auth);
+
+      do_get_info();
+    }
+   
+  }
+  
+  function login_(account,password){
+    
+    var loginJSONObject = {'name':account,'password':password };
+    login_options= {
+        url: host + '/bim/api/staff/login',
+        method: "POST",
+        headers: {
+            'User-Agent': 'request'
+        },
+        json: true,   // <--Very important!!!
+        body: loginJSONObject
+    };
+
+    request(login_options, login_callback);
+
+  }
+
+
+  
