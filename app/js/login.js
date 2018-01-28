9935005d2de9a0d79ea5c28e1000f6381f5efe95
DESKTOP-M2RDM21\akira <sunroad0418@gmail.com>
2018-01-28 11:09:13 +0800

    2018/1/28

--- app/js/login.js
+++ app/js/login.js
@@ -1,114 +1,137 @@
-var request = require('request');
-var host = 'http://lei-k.ddns.net:8081';
-var login_options ;
-var logout_options = {
-    url: host + '/bim/api/staff/logout',
-    method: "DELETE",
-    headers: {
-        'User-Agent': 'request',
-        'authorization' : auth
-      },
-      json: true,   // <--Very important!!!
-};
-var update_info_options = {
-    url: host + '/bim/api/staff/update',
-    method: "POST",
-    headers: {
-        'User-Agent': 'request',
-        'authorization' : auth
-      },
-      json: true,   // <--Very important!!!
-      body: info
-};
-var get_info_options = {
-    url: host + '/bim/api/staff/info',
-    method: "GET",
-    headers: {
-        'User-Agent': 'request',
-        'authorization' : auth
-      },
-      json: true,   // <--Very important!!!
-  };
-
-  function logout_callback(error, response, body) {
-    console.log("");
-    console.log("logout callback:");
-    console.log("http status code: " + response.statusCode);
-    console.log(body);
-  }
-
-  function do_logout(){
-    request(logout_options, logout_callback);
-  }
-
-  var info;
-
-  function update_info_callback(error, response, body){
-    console.log("");
-    console.log("update info callback:");
-    console.log("http status code: " + response.statusCode);
-    console.log(body);
-
-    do_logout();
-  }
-
-  function do_update_info(error, response, body){
-    info.emailAddress = 'aabbc!@gamil.com';
-    request(update_info_options, update_info_callback);
-  }
-
-  function get_info_callback(error, response, body){
-    console.log("");
-    console.log("get info callback:");
-    console.log("http status code: " + response.statusCode);
-    console.log(body);
-
-    info = body['content'];
-
-    do_update_info();
-  }
-
-  function do_get_info(error, response, body){
-    request(get_info_options, get_info_callback);
-  }
-
-  var auth;
-  var id;
-
-  function login_callback(error, response, body) {
-    console.log("login callback: ");
-    console.log("http status code: " + response.statusCode);
-    console.log(body);
-    
-    if (!error && response.statusCode == 200) {
-      // 獲取用戶id、token拼接後作為簽證(auth)備用
-      id = body['content'].id;
-      auth = id + '_' + body['content'].token;
-      console.log("auth: " + auth);
-
-      do_get_info();
+function bim_serveroption(url,method,headers,json,body){
+    this.url = url;
+    this.method = method;
+    this.headers = headers;    
+    this.body = body;  
+}
+function bim_userinfo(id,name,password,number,identity,emailAddress,createDate){
+  this.id = id;
+  this.name = name;
+  this.password = password;
+  this.number = number;
+  this.identity = identity;
+  this.emailAddress = emailAddress;
+  this.createDate = createDate;
+}
+function bim_app_window(){  
+  this.request = require('request');
+  this.host = 'http://140.118.127.140:8080';
+  this.permission = '/bim/api/';
+  this.api = '';
+  this.op = ''; //定義執行的操作
+  this.how = ''; //定義method
+  this.headers;
+  this.loginsucess = false;  
+  this.callbackfinish = false; 
+}
+bim_app_window.prototype.Send = function(obj){
+  let option;
+  console.log(this.op);
+  if (this.op === 'login') this.headers = { 'User-Agent': 'request' };
+  else this.headers = { 'User-Agent': 'request', 'authorization': localStorage.getItem('auth') };
+  if(obj)
+    option = new bim_serveroption(this.host + this.permission + this.api + this.op, this.how, this.headers, true, obj);
+  else
+    option =new bim_serveroption(this.host + this.permission + this.api + this.op, this.how, this.headers, true);  
+  if(!this.api||!this.op||!this.how||!this.headers)alert('send package error!');  
+  //this.request(option, this.bim_servercallback.bind(this));  //因為作為callback要綁定為現在的this 否則scope不會正確 
+  const url = new URL(this.permission + this.api + this.op ,this.host);
+  fetch(url,{method : this.how,body:obj, headers: this.headers ,credentials: "same-origin"}).then(this.testcallback,function(error){
+    error.message;
+  });
+}
+bim_app_window.prototype.testcallback = function(response){
+  console.log(response);
+}
+bim_app_window.prototype.bim_servercallback = function (error, response, body){
+  console.log("callback: ");
+  console.log("http status code: " + response.statusCode);
+  console.log(body);
+  console.log('code :' + body['code']);
+  console.log(this.op); 
+  if(this.op === 'login'){
+    let code = body['code'];
+    if(code == -1001)alert(body['message']);
+    else if(code == 100){
+      localStorage.setItem("auth",body['content'].id + '_' + body['content'].token );
+      console.log(localStorage.getItem('auth'));
+      waitForCallbacks();
+      this.callbackfinish = true;
+      this.loginsucess = true;
     }
-   
   }
-  
-  function login_(account,password){
-    
-    var loginJSONObject = {'name':account,'password':password };
-    login_options= {
-        url: host + '/bim/api/staff/login',
-        method: "POST",
-        headers: {
-            'User-Agent': 'request'
-        },
-        json: true,   // <--Very important!!!
-        body: loginJSONObject
-    };
+  else if(this.op === 'logout'){
+    localStorage.removeItem('auth');
+    this.loginsucess = false;
+  } 
+}
+bim_app_window.prototype.Reset = function(){
+  this.api = null;
+  this.op = null;
+  this.how = null;
+  this.headers = null;
+}
+bim_app_window.prototype.Login = function(account,password){
+  let obj = {'name':account,'password':password};
+  this.api = 'staff/';  
+  this.op = 'login';
+  this.how = 'POST';    
+  this.Send(obj); 
+}
+bim_app_window.prototype.checklogin = function(){
+  return this.loginsucess;
+}
+bim_app_window.prototype.GetInfo = function(){
+  this.api = 'staff/';
+  this.op = 'info';
+  this.how = 'GET';  
+  this.Send(null);
+}
+bim_app_window.prototype.Logout = function(){
+  this.api = 'staff/';
+  this.op = 'logout';
+  this.how = 'DELETE';  
+  this.Send(null);
+}
+bim_app_window.prototype.AddUser = function(name,password,number,id,email){
+  let obj = {'name' : name,
+             'password' : password,
+             'number' : number,
+             'identity' : id,
+             'emailAddress' : email};
+  this.api = 'admin/staff/';
+  this.op = 'add';
+  this.how = 'POST'; 
+  this.Send(obj);
+}
+bim_app_window.prototype.Update = function(info){
+  this.api = 'admin/staff/';
+  this.op = 'update';
+  this.how = 'POST';  
+  this.Send(info);
+}
+bim_app_window.prototype.GetStaffList = function(){
+  this.api = 'admin/staff/';
+  this.op = 'list';
+  this.how = 'GET'; 
+}
+bim_app_window.prototype.GetFormInfo = function(id){
+  this.api = 'form/template/';
+  this.op = 'info?id=' + Number(id);
+  this.how = 'GET';  
+  this.Send(null);
+}
+bim_app_window.prototype.AddFormTemplate = function(data){
+  this.api = 'form/template/';
+  this.op = 'add';
+  this.how = 'POST'; 
+  this.Send(data);
+}
+bim_app_window.prototype.GetFormList = function(){
+  this.api = 'form/template/';
+  this.op = 'list';
+  this.how = 'GET'; 
+  this.Send(null);
+}
 
-    request(login_options, login_callback);
-   
-
-  }
-
-  
-
-  
+var bim = new bim_app_window(); 
