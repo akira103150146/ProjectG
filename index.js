215716e71c27590d853991d8195195096285be8e
DESKTOP-M2RDM21\akira <sunroad0418@gmail.com>
2018-04-01 17:13:15 +0800

    Switch to webpack

---
+++ index.js
@@ -0,0 +1,278 @@
+/* eslint strict: 0 */
+"use strict";
+const electron = require('electron');
+const path = require('path')
+const url = require('url')
+const ipcMain = require('electron').ipcMain;
+const api = require('./src/js/lib/api.js');
+const rp = require('request-promise');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+const crashReporter = electron.crashReporter;
+const shell = electron.shell;
+let menu;
+let template;
+let mainWindow = null;
+const bim = new api()
+let lastpages = []
+const pagename = ['/assign.html', '/setting.html', '/form.html', '/member.html', '/info.html', '/post.html', '/history.html']
+let auth
+let win
+let result_win
+//crashReporter.start();
+
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  // if (win === null) {
+  //   createWindow()
+  // }
+})
+
+app.on('ready', () => {
+  win = new BrowserWindow({ width: 1920, height: 1080 })
+  result_win = new BrowserWindow({ width: 800, height: 600, parent: win, modal: true, show: false }) 
+
+  win.loadURL(`file://${__dirname}/src/login.html`)
+
+  win.on('closed', () => { win = null })
+  result_win.on('closed', () => { result_win = null })
+
+  ///////////////////////////////////////////////////////切換網頁//////////////////////////////////////////////////////
+  ipcMain.on('switch_page', (event, index) => {
+    lastpages.push(index)
+    win.loadURL(`file://${__dirname}/src/${pagename[index]}`)
+  })
+  ipcMain.on('last_page', (event) => {
+    win.loadURL(`file://${__dirname}/src/${pagename[5]}`)
+  })
+
+  ///////////////////////////////////////////////////////登入//////////////////////////////////////////////////////
+  ipcMain.on('login', (event, arg, arg2) => {
+    bim.Login(arg, arg2)
+
+    rp(bim.GetOption()).then((parseBody) => {
+      console.log(parseBody['code'])
+      if (parseBody['code'] == 100) {
+        var id = parseBody['content'].id
+        var token = parseBody['content'].token
+        auth = id + '_' + token
+        bim.SetAuth(auth)
+        win.webContents.send('reply-login', id, arg)//初始化使用者資料
+        win.loadURL(`file://${__dirname}/src/post.html`)
+        //result_win.loadURL('file://' + __dirname + '/bind_device.html')
+      }
+      else {
+        console.log('帳號或密碼錯誤')
+      }
+      bim.Reset()
+    }).catch((err) => {
+      console.log(err)
+    })
+  })
+
+  ///////////////////////////////////////////////////////登出//////////////////////////////////////////////////////
+  ipcMain.on('logout', () => {
+    bim.Logout()
+    rp(bim.GetOption()).then((parseBody) => {
+      console.log(parseBody)
+      bim.Reset()
+      win.loadURL('file://' + __dirname + '/index.html')
+    }).catch((err) => {
+      //console.log(err)
+    })
+  })
+
+  ///////////////////////////////////////////////////////開啟小視窗//////////////////////////////////////////////////////
+  ipcMain.on('toggle-result', function (event, which, data) {
+    console.log('call')
+    if (!result_win) {
+      result_win = new BrowserWindow({ width: 800, height: 600, parent: win, modal: true, show: false })
+      result_win.loadURL('file://' + __dirname + '/bind_device.html')
+      result_win.webContents.openDevTools()
+      result_win.on('closed', () => { result_win = null })
+    }
+    if (result_win.isVisible()) {
+      if (which === 'bind')
+        win.webContents.send('bind_devices', data)//送綁定資料到main window
+      result_win.hide()
+    }
+    else {
+      result_win.webContents.send('update-bind-list', data)//送綁定資料到sub window
+      result_win.show()
+      result_win.reload()
+    }
+
+  })
+
+  ///////////////////////////////////////////////////////更新//////////////////////////////////////////////////////
+  ipcMain.on('update', (event, which, id, body, tag) => {
+    console.log('update')
+    console.log('tag :' + tag)
+    if (which === 'member') {
+      bim.UpdateUser(id, body)
+    }
+    else if (which === 'device') {
+      bim.UpdateDevice(id, body)
+    }
+    else if (which === 'form') {
+      bim.UpdateFormTemplate(id, body)
+    }
+    else if (which === 'post') {
+      bim.UpdatePost(id, body)
+    }
+    else if (which === 'info') {
+      bim.UpdateCpn(id, body)
+    }
+    else if (which === 'Sdl') {
+      bim.UpdateSdl(id, body)
+    }
+    else {
+      console.log('which is not defined !')
+    }
+
+    rp(bim.GetOption()).then((parseBody) => {
+      console.log(parseBody)
+      bim.Reset()
+    }).catch((err) => {
+      //console.log(err)
+    })
+  })
+
+  ///////////////////////////////////////////////////////新增//////////////////////////////////////////////////////
+  ipcMain.on('add', (event, which, body, tag) => {
+    console.log('add')
+    console.log('tag :' + tag)
+    let path = ''
+
+    if (which === 'member') {
+      bim.AddUser(body)
+      path = 'updatecell-member'
+    }
+    else if (which === 'device') {
+      bim.AddDevice(body)
+      path = 'updatecell-device'
+    }
+    else if (which === 'form') {
+      bim.AddFormTemplate(body)
+      path = 'update-form'
+    }
+    else if (which === 'post') {
+      bim.AddPost(body)
+      path = 'update-post'
+    }
+    else if (which === 'assign') {
+      bim.Assign_Form(body)
+    }
+    else if (which === 'info') {
+      bim.AddCpn(body)
+    }
+    else if (which === 'Cpn-type') {
+      bim.AddCpnType(body)
+      path = 'update-Cpn-type'
+    }
+    else if (which === 'Sdl') {
+      bim.AddSdl(body)
+      path = 'update-Sdl'
+    }
+    else {
+      console.log('which is not defined')
+      console.log(which)
+    }
+    rp(bim.GetOption()).then((parseBody) => {
+      console.log(parseBody)
+      event.sender.send(path, parseBody.content, tag)
+      bim.Reset()
+    }).catch((err) => {
+      //console.log(err)
+    })
+  })
+
+  ///////////////////////////////////////////////////////刪除//////////////////////////////////////////////////////
+  ipcMain.on('remove', (event, which, body) => {
+    console.log('remove')
+    console.log(which)
+    if (which === 'member') {
+      bim.RemoveUser(body)
+    }
+    else if (which === 'device') {
+      bim.RemoveDevice(body)
+    }
+    else if (which === 'form') {
+      bim.RemoveFormTemplate(body)
+    }
+    else if (which === 'post') {
+      bim.RemovePost(body)
+    }
+    else if (which === 'info') {
+      bim.RemoveCpn(body)
+    }
+    else {
+      console.log('which is not defined')
+      console.log(which)
+    }
+    rp(bim.GetOption()).then((parseBody) => {
+      console.log(parseBody)
+      bim.Reset()
+    }).catch((err) => {
+      //console.log(err)
+    })
+
+  })
+
+  ///////////////////////////////////////////////////////顯示網頁//////////////////////////////////////////////////////
+  ipcMain.on('ready-to-show', (event, which) => {
+    console.log('ready-to-show')
+    let back_path = ''
+    if (which === 'member') {
+      console.log('ready-member')
+
+      bim.GetStaffList()
+      back_path = 'reply-member'
+    }
+    else if (which === 'device') {
+      console.log('ready-device')
+      bim.GetDeviceList()
+      back_path = 'reply-device'
+    }
+    else if (which === 'form') {
+      bim.GetFormTemplateList()
+      back_path = 'reply-form'
+    }
+    else if (which === 'history') {
+      bim.GetFormList()
+      back_path = 'reply-history'
+    }
+    else if (which === 'post') {
+      bim.GetPostList()
+      back_path = 'reply-post'
+    }
+    else if (which === 'Cpn-type') {
+      bim.GetCpnTypeList()
+      back_path = 'reply-Cpn-type'
+    }
+    else if (which === 'Cpn') {
+      bim.GetCpnList()
+      back_path = 'reply-Cpn'
+    }
+    else if (which === 'Sdl') {
+      bim.GetSdlList()
+      back_path = 'reply-Sdl'
+    }
+    else {
+    }
+    rp(bim.GetOption()).then((parseBody) => {
+      console.log(parseBody)
+      event.sender.send(back_path, parseBody['content'])
+      bim.Reset()
+    }).catch((err) => {
+      console.log(err)
+    })
+
+  })
+})
---
+++ src/js/lib/api.js
@@ -0,0 +1,248 @@
+"use strict";
+function bim_serveroption(url, method, headers, json, body) {
+  this.url = url
+  this.method = method
+  this.headers = headers
+  this.json = json
+  this.body = body
+}
+function bim_userinfo(id,name,password,number,identity,emailAddress,createDate){
+  this.id = id
+  this.name = name
+  this.password = password
+  this.number = number
+  this.identity = identity
+  this.emailAddress = emailAddress
+  this.createDate = createDate
+}
+function bim_app_window(){    
+  this.host = 'http://140.118.127.140:8080'
+  this.permission = '/bim/api/'
+  this.api = ''
+  this.op = '' //定義執行的操作
+  this.how = '' //定義method
+  this.headers 
+  this.option
+  this.auth
+}
+bim_app_window.prototype.Send = function(obj){   
+  if (this.op === 'login') 
+    this.headers = { 'User-Agent': 'request' }
+  else {
+    //console.log(this.auth)
+    this.headers = { 'User-Agent': 'request', 'authorization': this.auth }    
+  }
+  if(obj)
+    this.option = new bim_serveroption(this.host + this.permission + this.api + this.op, this.how, this.headers, true, obj)
+  else
+    this.option =new bim_serveroption(this.host + this.permission + this.api + this.op, this.how, this.headers, true,null)  
+  if(!this.api||!this.op||!this.how||!this.headers)alert('send package error!')   
+}
+//////////////////////////////////////主要操作///////////////////////////////////////////
+bim_app_window.prototype.Reset = function(){
+  this.api = null
+  this.op = null
+  this.how = null
+  this.headers = null
+  this.option = null
+  console.log('reset')
+}
+bim_app_window.prototype.GetOption = function(){
+  return this.option
+}
+bim_app_window.prototype.SetAuth = function(value){
+  this.auth = value
+}
+//////////////////////////////////////登入///////////////////////////////////////////
+bim_app_window.prototype.Login = function(account,password){
+  let obj = {'number':account,'password':password}
+  this.api = 'staff/'  
+  this.op = 'login'
+  this.how = 'POST'   
+  this.Send(obj) 
+}
+bim_app_window.prototype.Logout = function(){
+  this.api = 'staff/'
+  this.op = 'logout'
+  this.how = 'DELETE'  
+  this.Send(null)
+}
+//////////////////////////////////////工作人員操作///////////////////////////////////////////
+bim_app_window.prototype.AddUser = function(value){
+  let obj = {'name' : value.name,
+             'password' : value.password,
+             'number' : value.number,
+             'identity' : value.id,
+             'emailAddress': value.emailAddress}
+  this.api = 'admin/staff/'
+  this.op = 'add'
+  this.how = 'POST' 
+  this.Send(obj)
+}
+bim_app_window.prototype.UpdateUser = function(id,info){
+  this.api = 'admin/staff/'
+  this.op = 'update?id=' + id 
+  this.how = 'POST'  
+  this.Send(info)
+}
+bim_app_window.prototype.RemoveUser = function(id){
+  this.api = 'admin/staff/'
+  this.op = 'remove?id=' + id
+  this.how = 'DELETE'
+  this.Send(null)
+}
+bim_app_window.prototype.GetStaffList = function(){
+  this.api = 'admin/staff/'
+  this.op = 'list'
+  this.how = 'GET'
+  this.Send(null) 
+}
+//////////////////////////////////////表單操作///////////////////////////////////////////
+bim_app_window.prototype.GetFormTemplateList = function () {
+  this.api = 'form/template/'
+  this.op = 'list'
+  this.how = 'GET'
+  this.Send(null)
+}
+bim_app_window.prototype.AddFormTemplate = function(info){
+  this.api = 'admin/form/template/'
+  this.op = 'add'
+  this.how = 'POST' 
+  this.Send(info)
+}
+bim_app_window.prototype.UpdateFormTemplate = function(id,info){
+  this.api = 'admin/form/template/'
+  this.op = 'update?id=' + id
+  this.how = 'POST'
+  this.Send(info)
+}
+bim_app_window.prototype.RemoveFormTemplate = function(id){
+  this.api = 'admin/form/template/'
+  this.op = 'remove?id=' + id
+  this.how = 'DELETE'
+  this.Send(null)
+}
+bim_app_window.prototype.GetFormList = function(){
+  this.api = 'form/'
+  this.op = 'list'
+  this.how = 'GET'
+  this.Send(null)
+}
+//////////////////////////////////////設備操作///////////////////////////////////////////
+bim_app_window.prototype.GetDeviceList = function(){
+  this.api = 'device/'
+  this.op = 'list'
+  this.how = 'GET'
+  this.Send(null)
+}
+bim_app_window.prototype.AddDevice = function(info){
+  let obj = {'name':info.name,
+              'description': info.Description,
+              'position': info.position,
+              'tagName': info.tagName
+  }
+  this.api = 'admin/device/'
+  this.op = 'add'
+  this.how = 'POST'
+  this.Send(obj)
+}
+bim_app_window.prototype.RemoveDevice = function(id){
+  this.api = 'admin/device/'
+  this.op = 'remove?id=' + id 
+  this.how = 'DELETE'
+  this.Send(null)
+}
+bim_app_window.prototype.UpdateDevice = function(id,info){
+  this.api = 'admin/device/'
+  this.op = 'update?id=' + id
+  this.how = 'POST'
+  this.Send(info)
+}
+//////////////////////////////////////公佈欄操作///////////////////////////////////////////
+bim_app_window.prototype.GetPostList = function(){
+  this.api = 'post/'
+  this.op = 'list'
+  this.how = 'GET'
+  this.Send(null)
+}
+bim_app_window.prototype.AddPost = function(info){
+  this.api = 'admin/post/'
+  this.op = 'add'
+  this.how = 'POST'
+  this.Send(info)
+}
+bim_app_window.prototype.UpdatePost = function(id,info){
+  this.api = 'admin/post/'
+  this.op = 'update?id=' + id
+  this.how = 'POST'
+  this.Send(info)
+}
+bim_app_window.prototype.RemovePost = function(id){
+  this.api = 'admin/post/'
+  this.op = 'remove?id=' + id
+  this.how = 'DELETE'
+  this.Send(null)
+}
+//////////////////////////////////////零件操作///////////////////////////////////////////
+bim_app_window.prototype.GetCpnList = function(){
+ this.api = 'component/'
+ this.op = 'list'
+ this.how = 'GET'
+ this.Send(null)
+}
+bim_app_window.prototype.AddCpn = function(info){
+  this.api = 'admin/component/'
+  this.op = 'add'
+  this.how = 'POST'
+  this.Send(info)
+}
+bim_app_window.prototype.RemoveCpn = function(id){
+  this.api = 'admin/component/'
+  this.op = 'remove?id=' + id
+  this.how = 'DELETE'
+  this.Send(null)
+}
+bim_app_window.prototype.UpdateCpn = function(id,info){
+  this.api = 'admin/component/'
+  this.op = 'update?id=' + id
+  this.how = 'POST'
+  this.Send(info)
+}
+bim_app_window.prototype.GetCpnTypeList = function(){
+  this.api = 'component_type/'
+  this.op = 'list'
+  this.how = 'GET'
+  this.Send(null)
+}
+bim_app_window.prototype.AddCpnType = function(info){
+  this.api = 'admin/component_type/'
+  this.op = 'add'
+  this.how = 'POST'
+  this.Send(info)
+}
+
+bim_app_window.prototype.GetSdlList = function(){
+  this.api = 'schedule/'
+  this.op = 'list'
+  this.how = 'GET'
+  this.Send(null)
+}
+bim_app_window.prototype.AddSdl = function(info){
+  this.api = 'admin/schedule/'
+  this.op = 'add'
+  this.how = 'POST'
+  this.Send(info)
+}
+bim_app_window.prototype.RemoveSdl = function(id){
+  this.api = 'admin/schedule/'
+  this.op = 'remove?id=' + id
+  this.how = 'DELETE'
+  this.Send(null)
+}
+bim_app_window.prototype.UpdateSdl = function(id,info){
+  this.api = 'admin/schedule/'
+  this.op = 'update?id=' + id 
+  this.how = 'POST'
+  this.Send(info)
+}
+module.exports = bim_app_window
